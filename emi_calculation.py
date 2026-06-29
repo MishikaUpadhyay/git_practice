f63b600d2fd9577c3c60f3403892d58dcb72d03a
Mishika Upadhyay <upadhyaymishika6@gmail.com>
2026-06-29 19:41:29 +0530

    Add emi calculation

---
+++ emi_calculation.py
@@ -0,0 +1,8 @@
+p=int(input("enter principal amount:"))
+r=int(input("enter rate of interest:"))
+t=int(input("enter time:"))
+S=p*r*t/100
+print("simple interest:",S)
+"""total=p+S
+emi=total/t
+print("EMI=",emi)"""
